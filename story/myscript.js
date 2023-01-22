bf1db2b14bc822dd39f0ba42560db7f625886fa7
MegNoonan <114246973+MegNoonan@users.noreply.github.com>
2023-01-22 16:45:33 -0500

    Add multiple markers

--- story/myscript.js
+++ story/myscript.js
@@ -1,47 +1,67 @@
 // Based on the info windows with a max width template 
+// Multiple markers modified from https://www.codexworld.com/google-maps-with-multiple-markers-using-javascript-api/
+
 function initMap() {
-  const ireland = { lat: 53.316, lng: -7.698 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  
+  //const ireland = { lat: 53.316, lng: -7.698 };
+  var bounds = new google.maps.LatLngBounds();
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
     center: ireland,
      mapTypeId: google.maps.MapTypeId.TERRAIN
   });
-  const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Ireland</h1>' +
+  var markers = [
+    ['Dingle, Ireland', 53.316, -7.698],
+    ['Inverness, Scotland', 57.456, -4.221]]
+  
+  var infoWindowContent =
+    ['<div id="content">' +
+    '<h1>Dingle, Ireland</h1>' +
+    '<h3> Travel Dates: August 23 to December 29' +
     '<div id="bodyContent">' +
-    "<p><b>Ireland</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Heritage Site.</p>" +
-    "</div>" +
-    "</div>";
-  const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    ariaLabel: "Ireland",
-  });
-  const marker = new google.maps.Marker({
-    position: ireland,
-    map,
-    title: "Ireland (Ayers Rock)",
-  });
+    '<p> My favourite part of travelling in Ireland was this little bar we went to near Killarney. We played pool with the local colleges engineering' +
+      ' students and learned a lot about the country. There was a dog named Molly that would sit right at the bar. </p>'+
+    '</div>'],
+        ['<div id="content">' +
+    '<h1>Inverness, Scotland</h1>' +
+    '<h3> Travel Dates: August 23 to December 29' +
+    '<div id="bodyContent">' +
+    '<p> My favourite part of travelling in Ireland was this little bar we went to near Killarney. We played pool with the local colleges engineering' +
+      ' students and learned a lot about the country. There was a dog named Molly that would sit right at the bar. </p>'+
+    '</div>']
+  ];
+  
+      // Add multiple markers to map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Place each marker on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+      
+// Add info window to marker    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
 
-  marker.addListener("click", () => {
-    infowindow.open({
-      anchor: marker,
-      map,
+        // Center the map to fit all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Set zoom level
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
     });
-  });
 }
 
 window.initMap = initMap;
-
 
