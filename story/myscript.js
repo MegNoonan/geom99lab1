67c097179378daf20fb853d46f6c6dc1f13b6f34
MegNoonan <114246973+MegNoonan@users.noreply.github.com>
2023-01-22 18:46:44 -0500

    Update myscript.js

--- story/myscript.js
+++ story/myscript.js
@@ -10,7 +10,24 @@ function initMap() {
         mapTypeId: 'hybrid'
     };
    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-                             
+    
+     // Adding some hiking trail polylines 
+       const flightPlanCoordinates = [
+    { lat: 37.772, lng: -122.214 },
+    { lat: 21.291, lng: -157.821 },
+    { lat: -18.142, lng: 178.431 },
+    { lat: -27.467, lng: 153.027 },
+  ];
+  const flightPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  flightPath.setMap(map);
+     
   //const ireland = { lat: 53.316, lng: -7.698 };
   //var bounds = new google.maps.LatLngBounds();
   //var map = new google.maps.Map(document.getElementById("map"), {
